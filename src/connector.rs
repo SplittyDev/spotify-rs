b052f8bd0bddcddd52281eed27fdc38b6e4b30b8
Splitty <splittydev@gmail.com>
2016-11-23 02:05:58 +0100

    Refactor SpotifyConnector
    - Clean up messy code

--- src/connector.rs
+++ src/connector.rs
@@ -28,39 +28,35 @@ pub struct SpotifyConnector {
 /// Implements `SpotifyConnector`.
 impl SpotifyConnector {
     pub fn new(host: String) -> Result<SpotifyConnector, SpotifyError> {
-        match Client::new() {
-            Ok(client) => {
-                let token_req = client.get(SPOTIFY_URL_TOKEN)
-                    .header(UserAgent(USER_AGENT.into()))
-                    .send();
-                match token_req {
-                    Ok(mut token_resp) => {
-                        let mut json_str = String::new();
-                        match token_resp.read_to_string(&mut json_str) {
-                            Ok(_) => {
-                                match self::json::parse(json_str.as_ref()) {
-                                    Ok(json) => {
-                                        match json["t"].as_str() {
-                                            Some(token) => {
-                                                Ok(SpotifyConnector {
-                                                    host: host,
-                                                    client: client,
-                                                    token: token.into(),
-                                                })
-                                            }
-                                            None => Err(SpotifyError::InvalidToken),
-                                        }
-                                    }
-                                    Err(error) => Err(SpotifyError::TokenParseError(error)),
-                                }
-                            }
-                            Err(error) => Err(SpotifyError::TokenIOError(error)),
-                        }
-                    }
-                    Err(error) => Err(SpotifyError::ReqwestError(error)),
+        let client = match Client::new() {
+            Ok(client) => client,
+            Err(error) => return Err(SpotifyError::ReqwestError(error)),
+        };
+        let token = {
+            let mut token_resp =
+                match client.get(SPOTIFY_URL_TOKEN).header(UserAgent(USER_AGENT.into())).send() {
+                    Ok(resp) => resp,
+                    Err(err) => return Err(SpotifyError::ReqwestError(err)),
+                };
+            let json = {
+                let mut json_str = String::new();
+                if let Err(error) = token_resp.read_to_string(&mut json_str) {
+                    return Err(SpotifyError::TokenIOError(error));
                 }
+                match self::json::parse(json_str.as_ref()) {
+                    Ok(data) => data,
+                    Err(err) => return Err(SpotifyError::TokenParseError(err)),
+                }
+            };
+            match json["t"].as_str() {
+                Some(token) => token.to_owned(),
+                None => return Err(SpotifyError::InvalidToken),
             }
-            Err(error) => Err(SpotifyError::ReqwestError(error)),
-        }
+        };
+        Ok(SpotifyConnector {
+            host: host,
+            client: client,
+            token: token.into(),
+        })
     }
 }
